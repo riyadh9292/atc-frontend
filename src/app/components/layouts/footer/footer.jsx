0062f2ce1563239de1fd671b1d363f6877a1ad56
sifatfaysal <sifatfaysall@gmail.com>
2023-07-08 21:12:27 +0600

    design aboutUs component

--- src/app/components/layouts/footer/footer.jsx
+++ src/app/components/layouts/footer/footer.jsx
@@ -3,7 +3,7 @@ import Button from "../../atoms/Button/Button";
 
 export default function Footer() {
   return (
-    <section className="bg-arabica">
+    <section className="bg-golden">
       <div className="container">
         <div className="flex justify-center items-center h-12">
           <p className="text-white">All rights reserved By ATC © 2023</p>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Layout from "./components/layouts/layout/layout";
-import Hero from "./components/molecules/Hero/hero";
+import AboutSection from "./components/molecules/About/AboutSection";
+import Hero from "./components/molecules/Hero/HeroSection";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <AboutSection/>
     </Layout>
   );
 }
