2e7690ea63261a37de79da1ba5cb1183c9cbb660
riyadh9292 <mrriyadh66@gmail.com>
2023-07-10 00:08:21 +0600

    updated.

---
+++ src/app/components/pages/public/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "@/app/components/molecules/banner/Banner";
+
+const Landing = () => {
+  return (
+    <section>
+      <Banner />
+    </section>
+  );
+};
+
+export default Landing;
--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Hero from "./components/molecules/Hero/HeroSection";
 import MissionSection from "./components/molecules/Mission/MissionSection";
 import ServiceSection from "./components/molecules/Service/ServiceSection";
 import VisionSection from "./components/molecules/Vision/VisionSection";
+import Landing from "./components/pages/public/landing/Landing";
 
 export default function Home() {
   return (
     <Layout>
-      <Hero />
-      <AboutSection/>
+      <Landing />
+      {/* <Hero />
+      <AboutSection/> */}
       {/* <MissionSection/>
       <VisionSection/>
       <ServiceSection/> */}
