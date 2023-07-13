9d4797c0fa34e189f17d4bc8a4affcd069579f1e
riyadh9292 <mrriyadh66@gmail.com>
2023-07-14 01:07:09 +0600

    color updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,20 @@ module.exports = {
         graphite: "#1B2430",
         arabica: "#0A642B",
         golden: "#D2AF26",
-        fanta: "#F38222",
+        // fanta: "#F38222",
+        fanta: "#0A642B",
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
         serif: ["Poppins", "serif"],
-        handlee: ['handlee', 'sans-serif'],
+        handlee: ["handlee", "sans-serif"],
       },
       backgroundImage: {
-        'section': "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/image/forex__image.jpg')",
-        'parallax': "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/image/bg__image.jpg')",
-        'contact': "url('/image/contact__bg.jpg'),",
+        section:
+          "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('/image/forex__image.jpg')",
+        parallax:
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/image/bg__image.jpg')",
+        contact: "url('/image/contact__bg.jpg'),",
       },
     },
   },
